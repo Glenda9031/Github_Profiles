38d607328a64cc7830a08461d04a5d8c00645065
Glenda Reed <glendareed0807@sbcglobal.net>
2023-05-25 18:25:36 -0500

    Update style.css and script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const APIUAL = 'https://api.github.com/users/'
+
+const main = document.getElementById('main')
+const for0 = document.getElementById('form')
+const search = document.getElementById('search')
+
+
+async function getUser(userName) {
+    try {
+        const { data }
+    }
+}
